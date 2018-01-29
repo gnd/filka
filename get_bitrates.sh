dba4ddf2e1f4ec418b244fd31ee769d9792b2fbe
gnd <gnd@itchybit.org>
2018-01-29 18:50:58 +0100

    overhauled to use mod_io

---
+++ get_bitrates.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# 
+# create a list of all current videos and their bitrates
+########################################################
+
+if [[ -z $1 ]]; then
+	echo "Usage $0 <logfile> <rootdir>"
+	exit
+fi
+if [[ -z $2 ]]; then
+	echo "Usage $0 <logfile> <rootdir>"
+	exit
+fi
+
+LOG_FILE=$1
+ROOT_DIR=$2
+rm video_bitrates.txt
+
+for FILE in `cat $LOG_FILE |grep mp4 |grep -v dl.php|awk {'print $7;'}|sort|uniq`
+do
+	if [ -f $vid ]; then
+		VIDEO=$ROOT_DIR$FILE
+		TYPE=`file $VIDEO|awk {'print $4;'}`
+		if [[ $TYPE == "MP4" ]]; then
+			BITRATE=`avprobe $VIDEO 2>&1 |grep bitrate|awk {'print $6;'}`
+			BITS=$(($BITRATE * 1024))
+			echo "$FILE $BITS" >> video_bitrates.txt
+		fi
+	fi
+done
--- get_stats.py
+++ get_stats.py
@@ -1,57 +1,49 @@
-#!/usr/bin/python
-#
-# ## get_stats.py
-#
-# parses apache logfiles for number of visits / seconds spent watching / data transferred
-# logfiles are in the form (use split.sh):
-#     Jan_Year.log
-#     Feb_Year.log
-#     March_Year.log
-#     ...
-# 
-# where the LogFormat is:
-#     LogFormat "%v %h %l %u %t \"%r\" %>s %b \"%{Referer}i\""
-#
-#
-# ## Usage
-#
-# ./get_stats.py <day | month | all> <selectors>
-# ./get_stats.py day <year> <month> <day>
-# ./get_stats.py month <year> <month>
-# ./get_stats.py all <year>
-# eg.: ./get_stats.py day 2016 06 06
-#
-#
-# ## Outputs
-#
-# visits:
-#   - a visit is a hit from an ip on a .mp4 file at a specific time
-#   - if another hit is seen from the same ip within 5400 seconds of the first encounter
-#     it is considered to be a part of the same visit
-#   - if a hit from the same ip is seen later than 5400 seconds from the last visit,
-#     it is considered to be a new visit
-#
-# seconds:
-#   - in the absence of better data we count the time spent watching as:
-#     seconds = time of last hit during a given visit - time of first hit during a given visit
-#
-# data:
-#   - likewise since apache with LogFormat %b shows usually the whole filesize as transferred
-#     (even when the transfer was interrupted) we count the data transmitted as:
-#     duration of visit * bitrate of the watched video
-#   - this is always a lower bound of data transmitted as various browsers employ various 
-#     caching strategies, and might actually download much more data
-#   - a better step in determining the data transferred is using %O of mod_logio (next year)
-#   - the bitrates are stored in the file video_bitrates.txt which is generated using avprobe
-#
-##############################################################################################
+#!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+
+""" get_stats.py
+
+    This parses apache logfiles for number of visits / seconds spent watching / data transferred
+    logfiles are in the form (use split.sh):
+        Jan_Year.log
+        Feb_Year.log
+        March_Year.log
+        ...
+
+    Where the LogFormat is:
+        LogFormat "%v %h %l %u %t \"%r\" %>s %b %O \"%{Referer}i\""
+
+    Usage:
+        ./get_stats.py <day | month | all> <selectors>
+        ./get_stats.py day <year> <month> <day>
+        ./get_stats.py month <year> <month>
+        ./get_stats.py all <year>
+        eg.: ./get_stats.py day 2016 06 06
+
+    Outputs:
+        visits:
+            - a visit is a hit from an ip on a .mp4 file at a specific time
+            - if another hit is seen from the same ip within 5400 seconds of the first encounter
+            it is considered to be a part of the same visit
+            - if a hit from the same ip is seen later than 5400 seconds from the last visit,
+            it is considered to be a new visit
+
+        seconds:
+            - in the absence of better data we count the time spent watching as:
+            seconds = time of last hit during a given visit - time of first hit during a given visit
+
+        data:
+            - no need to account for data for a given visit, it is enough to gather %O for the whole day
+
+    gnd, 2017 - 2018
+"""
 
 import sys
 import time
 import datetime
 from os.path import basename
 
-days = {'Jan': 31, 'Feb': 28, 'Mar': 31, 'Apr': 30, 'May': 31, 'Jun': 30, 'Jul': 30, 'Aug': 31, 'Sep': 30, 'Oct': 31, 'Nov': 30, 'Dec': 31} 
+days = {'Jan': 31, 'Feb': 28, 'Mar': 31, 'Apr': 30, 'May': 31, 'Jun': 30, 'Jul': 30, 'Aug': 31, 'Sep': 30, 'Oct': 31, 'Nov': 30, 'Dec': 31}
 months = ['Jan', 'Feb','Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 #
@@ -69,7 +61,7 @@ def get_month_loglines(logfile):
 def get_day_loglines(day, month_loglines):
     daylines = []
     for line in month_loglines:
-        if (day in line) and ("mp4" in line) and not ('HTTP/1.1" 404' in line) and (len(line.split()) == 11):
+        if (day in line) and ("mp4" in line) and not ('HTTP/1.1" 404' in line) and (len(line.split()) == 12):
             daylines.append(line)
     return daylines
 
@@ -80,7 +72,7 @@ def get_day_loglines(day, month_loglines):
 def get_day_ips(day_loglines):
     ips = []
     for line in day_loglines:
-        if (line.split()[0] not in ips and (len(line.split()) == 11)):
+        if (line.split()[0] not in ips and (len(line.split()) == 12)):
             ips.append(line.split()[0])
     return ips
 
@@ -97,45 +89,51 @@ def get_bitrates():
     return bitrates
 
 #
-# gets count ips / seconds / bytes for a given day
+# gets count ips / seconds for a given day
 #
-def get_day_totals(ips, loglines, bitrates, is_day):
+def get_day_visits(ips, loglines, is_day):
     visits = 0;
     seconds = 0
-    data = 0
     for ip in ips:
         last = 0
         for line in loglines:
             iptime = time.mktime(datetime.datetime.strptime(line.split()[3].replace("[",""), "%d/%b/%Y:%H:%M:%S").timetuple())
-            video = basename(line.split()[6])
-            bitrate = 0
-            if video in bitrates:
-                bitrate = bitrates[video] / 8
-            # this searches for the length of a given visit
+            # this searches for the length of a given visit - if no last, its the moment when the IP shows up
             if (ip in line) and (last == 0):
                 visits += 1
                 first = iptime
                 last = iptime
                 first_str = line.split()[3].replace("[","")
-                last_str = line.split()[3].replace("[","")              
+                last_str = line.split()[3].replace("[","")
+            # we still see the IP, its not the first time and its part of the same visit
             if (ip in line) and (iptime - last <= 5400) and (iptime - last > 0):
                 last = iptime
                 last_str = line.split()[3].replace("[","")
+            # in case we see ip after 5400 seconds, make it a new visit and finish & account for the old one
             if (ip in line) and (iptime - last > 5400) and (last != 0):
-                #print "ip: %s first: %s last: %s seconds: %d data: %d" % (ip, first_str, last_str, (last - first), ((last - first) * bitrate))
-                data += (last - first) * bitrate
                 seconds += (last - first)
                 visits+=1
                 first = iptime
                 first_str = line.split()[3].replace("[","")
                 last_str = line.split()[3].replace("[","")
                 last = iptime
+        # otherwise no more hits from IP, account for the visit
         seconds += (last - first)
-        data += (last - first) * bitrate
         if is_day:
-            print "IP: %s first: %s last: %s seconds: %d data: %d" % (ip, first_str, last_str, (last - first), ((last - first) * bitrate))  
-    return (visits, seconds, data)
+            print "IP: %s first: %s last: %s seconds: %d" % (ip, first_str, last_str, (last - first))
+    return (visits, seconds)
 
+#
+# gets data sent for a given day
+#
+def get_day_data(loglines, is_day):
+    data = 0
+    for line in loglines:
+        bytes_out = int(line.split()[10])
+        data += bytes_out
+    if is_day:
+        print "Transferred: %d" % (data)
+    return (data)
 
 #
 # main()
@@ -155,24 +153,20 @@ if scope == 'day':
     year = sys.argv[2]
     month = int(sys.argv[3])-1
     daynum = sys.argv[4]
-
     logfile = "%s_%s.log" % (months[month], year)
     monthlines = get_month_loglines(logfile)
     day = "%02d/%s" % (int(daynum), months[month])
     daylines = get_day_loglines(day, monthlines)
     ips = get_day_ips(daylines)
-    bitrates = get_bitrates()
-
-    (visits, seconds, data) = get_day_totals(ips, daylines, bitrates, True)
-    print "Visits: %d Seconds: %d Data: %f" % (visits, seconds, data)
+    (visits, seconds) = get_day_visits(ips, daylines, True)
+    data = get_day_data(daylines, True)
+    print "Visits: %d Seconds: %d Data: %d" % (visits, seconds, data)
 
 elif scope == 'month':
     year = sys.argv[2]
     month = int(sys.argv[3])-1
-    
     logfile = "%s_%s.log" % (months[month], year)
     monthlines = get_month_loglines(logfile)
-    bitrates = get_bitrates()
     total_visits = 0
     total_seconds = 0
     total_data = 0
@@ -180,23 +174,22 @@ elif scope == 'month':
         day = "%02d/%s" % (daynum, months[month])
         daylines = get_day_loglines(day, monthlines)
         ips = get_day_ips(daylines)
-        (visits, seconds, data) = get_day_totals(ips, daylines, bitrates, False)
+        (visits, seconds) = get_day_visits(ips, daylines, False)
+        data = get_day_data(daylines, False)
         total_visits += visits
         total_seconds += seconds
         total_data += data
-        print "Day %d Visits: %d Seconds: %d Data: %f" % (daynum, visits, seconds, data)
-    print "Visits: %d Seconds: %d Data: %f" % (total_visits, total_seconds, total_data)
+        print "Day %d Visits: %d Seconds: %d Data: %d" % (daynum, visits, seconds, data)
+    print "Visits: %d Seconds: %d Data: %d" % (total_visits, total_seconds, total_data)
 
 elif scope == 'all':
     year = sys.argv[2]
-
     total_visits = 0
     total_seconds = 0
-    total_data = 0    
+    total_data = 0
     for month in months:
         logfile = "%s_%s.log" % (month, year)
         monthlines = get_month_loglines(logfile)
-        bitrates = get_bitrates()
         month_visits = 0
         month_seconds = 0
         month_data = 0
@@ -204,15 +197,16 @@ elif scope == 'all':
             day = "%02d/%s" % (daynum, month)
             daylines = get_day_loglines(day, monthlines)
             ips = get_day_ips(daylines)
-            (visits, seconds, data) = get_day_totals(ips, daylines, bitrates, False)
+            (visits, seconds) = get_day_visits(ips, daylines, False)
+            data = get_day_data(daylines, False)
             month_visits += visits
             month_seconds += seconds
             month_data += data
-        print "%s Visits: %d Seconds: %d Data: %f" % (month, month_visits, month_seconds, month_data)
+        print "%s Visits: %d Seconds: %d Data: %d" % (month, month_visits, month_seconds, month_data)
         total_visits += month_visits
         total_seconds += month_seconds
         total_data += month_data
-    print "%s Visits: %d Seconds: %d Data: %f" % (month, total_visits, total_seconds, total_data)
+    print "%s Visits: %d Seconds: %d Data: %d" % (month, total_visits, total_seconds, total_data)
 
 else:
     print "Usage: %s <day | month | all> <selectors>" % (sys.argv[0])
