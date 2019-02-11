43eaa4d01465cbc2b95c2827a8cf003b342128f5
gnd <gnd@itchybit.org>
2019-02-11 13:13:29 +0100

    split update

--- split.sh
+++ split.sh
@@ -2,15 +2,24 @@
 #
 # splits Apache logfile into monthly parts
 ##########################################
+
 if [[ -z "$1" ]]; then
 	print "Usage: $0 <year>"
 	exit
+else
+	YEAR=$1
+	STATDIR="stats_"$YEAR
 fi
 
-YEAR=$1
-rm $YEAR"_access_log"
+## clean old data (if any)
+if [[ -d $STATDIR ]]; then
+	rm -rf $STATDIR/*
+else
+	mkdir $STATDIR
+fi
+
 MON=("Jan" "Feb" "Mar" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec");
 for m in ${MON[@]}
 do
-	cat ../apache_access_log|grep $m"/"$YEAR | tee -a $YEAR"_access_log" > $m"_"$YEAR".log"
+	cat ../apache_access_log|grep $m"/"$YEAR | tee -a $STATDIR/$YEAR"_access_log" > $STATDIR/$m"_"$YEAR".log"
 done
