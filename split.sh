225bf1dbd938b7d5c6e7c972258bce928ee37dbc
gnd <gnd@itchybit.org>
2023-07-17 22:10:58 +0200

    Adding ip exclude file to split script

--- split.sh
+++ split.sh
@@ -2,14 +2,21 @@
 #
 # splits Nginx logfile into monthly parts
 ##########################################
+USE_EXCLUDE=0
 
 if [[ -z "$1" ]]; then
-	print "Usage: $0 <year>"
+	print "Usage: $0 <year> [exclude_ips_file]"
 	exit
 else
 	YEAR=$1
 	STATDIR="stats_"$YEAR
 fi
+
+if [[ ! -z "$2" ]]; then
+	EXCLUDE_FILE=$2
+	USE_EXCLUDE=1
+	echo "Using IP exclude list from $EXCLUDE_FILE"
+else
 
 ## clean old data (if any)
 if [[ -d $STATDIR ]]; then
@@ -21,5 +28,9 @@ fi
 MON=("Jan" "Feb" "Mar" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec");
 for m in ${MON[@]}
 do
-	cat ../nginx_access_log|grep -a $m"/"$YEAR | tee -a $STATDIR/$YEAR"_access_log" > $STATDIR/$m"_"$YEAR".log"
+	if [[ $USE_EXCLUDE == "1" ]]; then
+		cat ../nginx_access_log|grep -a $m"/"$YEAR -vf $EXCLUDE_FILE | tee -a $STATDIR/$YEAR"_access_log" > $STATDIR/$m"_"$YEAR".log"
+	else
+		cat ../nginx_access_log|grep -a $m"/"$YEAR | tee -a $STATDIR/$YEAR"_access_log" > $STATDIR/$m"_"$YEAR".log"
+	fi
 done
